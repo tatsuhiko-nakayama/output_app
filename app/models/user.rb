c984b3d219e93c38617d29cb54288ef73b16e5d8
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-01 18:06:22 +0900

    Userモデルの作成
    - migration
    - association
    - validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :profile
+  has_many :items
+  has_many :comments
+  has_many :likes
+  has_many :relationships
+
+  validates :username, presence: true, uniqueness: { case_sensitive: true}
+
 end
