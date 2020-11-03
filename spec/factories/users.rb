6f73e14da663a0aab11178edabe0344edadbb324
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-03 10:46:14 +0900

    単体テスト(モデル）
    - User
    - Profile

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Name.initials(number: 2) }
+    email { Faker::Internet.free_email }
+    password { 'test1234' }
+    password_confirmation { password }
   end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'プロフィール登録' do
+    before do
+      @profile = FactoryBot.build(:profile)
+    end
+
+    context '登録がうまくいくとき' do
+      it 'すべて空でも登録できる' do
+        @profile.lastname = nil
+        @profile.firstname = nil
+        @profile.website = nil
+        @profile.intro
+        expect(@profile).to be_valid
+      end
+    end
+
+    context '登録がうまくいかないとき' do
+      it 'lastnameが21文字以上では登録できない' do
+        @profile.lastname = Faker::Name.initials(number: 21)
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include('姓は20文字以内で入力してください')
+      end
+
+      it 'firstnameが21文字以上では登録できない' do
+        @profile.firstname = Faker::Name.initials(number: 21)
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include('名は20文字以内で入力してください')
+      end
+
+      it 'introが201文字以上では登録できない' do
+        @profile.intro = Faker::Name.initials(number: 201)
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include('自己紹介は200文字以内で入力してください')
+      end
+    end
+
+  end
 end
