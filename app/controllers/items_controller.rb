df4f2c76300bf90b42d8d604d622da1bb0cdced3
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-03 18:30:50 +0900

    投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,37 @@ class ItemsController < ApplicationController
     if current_user
       @user = User.find(current_user.id)
     end
-    
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :tagbody, :body, :url, :status, :category_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,8 @@ class Category < ActiveHash::Base
   self.data = [
     { id:1, name: '本'},
     { id:2, name: 'Youtube'},
-    { id:3, name: '記事'},
-    { id:4, name: 'SNS'},
-    { id:5, name: 'イベント'},
-    { id:6, name: '映画'},
-    { id:7, name: '出来事'}
+    { id:3, name: '記事/SNS'},
+    { id:4, name: 'イベント'},
+    { id:5, name: '出来事'}
   ]
 end
