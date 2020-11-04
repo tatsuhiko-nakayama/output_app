31379dbfd346506f18d8c26d6e10c52901a99e02
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-04 10:54:26 +0900

    時刻を日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module OutputApp
     # 日本語の言語設定
     config.i18n.default_locale = :ja
 
+    # 日本時刻の設定
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
