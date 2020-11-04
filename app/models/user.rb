0e1c44ec5ebb4b62dc8fd1f7ef881d6dbdda39b8
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-05 01:10:58 +0900

    投稿リストのサイズ調整（バリデーション変更含む）

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,10 @@ class User < ApplicationRecord
   has_many :likes
   has_many :relationships, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: { case_sensitive: true}
+  validates :username, presence: true, uniqueness: { case_sensitive: true}, length: { maximum: 14 }
 
+  USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates_format_of :username, with: USERNAME_REGEX, message: 'は半角英数で入力してください'
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数のみで両方含めてください'
 end
