8f055e4f2eec4cf16154bc55a1e186a555ab6672
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-08 20:18:50 +0900

    ルーティングとコントローラーの初期設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to item_path(@comment.item_id)
+    else
+      redirect_to item_path(@comment.item_id)
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      redirect_to item_path(comment.item_id)
+    else
+      redirect_to item_path(comment.item.id)
+    end
+  end
+
+  def cooment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
