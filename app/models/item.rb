e05d0cd60bcf66d3c5a52776f75d0cb0de71875f
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-11 12:43:38 +0900

    item削除できない状態の解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
   has_many :commented_users, through: :comments, source: :user
-  has_many :item_tags
+  has_many :item_tags, dependent: :destroy
   has_many :tags, through: :item_tags
   belongs_to_active_hash :category
 
