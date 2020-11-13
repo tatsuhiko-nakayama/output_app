b7d8f2a22850725ff7622d4f0a55729b7b9018bd
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-13 16:08:53 +0900

    ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,4 +46,9 @@ class User < ApplicationRecord
       User.all
     end
   end
+
+  def self.guest
+    find(15)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :users, only: [:show] do
     get :search, on: :collection
     resource :profile, only: [:edit, :update]
