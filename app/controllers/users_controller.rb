2e40899cea00492b6a56bbbe5323a7e63d4c6f02
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-21 13:22:57 +0900

    ユーザーページ記事リストのページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @items = @user.items.open.order('created_at DESC')
-    @user_items = @user.items.order('created_at DESC')
+    @items = @user.items.open.order('created_at DESC').page(params[:page])
+    @user_items = @user.items.order('created_at DESC').page(params[:page])
   end
 
   def search
