99f12e39967f9f81fffdd8e442364333c4c65379
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-29 16:45:12 +0900

    通知画面のビュー調整

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,12 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(10)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(15)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
+    end
+    if user_signed_in?
+      @user = current_user
+      @my_items = Item.where(user_id: current_user.id).order('created_at DESC')
     end
   end
 end
