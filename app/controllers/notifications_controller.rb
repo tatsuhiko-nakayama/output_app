4ac18357ee017c23206d72007eba78448168ccbb
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-29 18:33:02 +0900

    通知有無によるマーク表示の実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(15)
+    @notifications = current_user.passive_notifications.where.not(visitor_id: current_user.id).page(params[:page]).per(15)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
