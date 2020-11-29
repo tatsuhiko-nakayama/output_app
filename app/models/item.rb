791ad2dec1a6b6f67d706adfbc0c4699c7d22db8
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-11-29 11:44:07 +0900

    各モデルへのメソッド定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -50,4 +50,41 @@ class Item < ApplicationRecord
       Item.all
     end
   end
+
+  def create_notification_like(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        item_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+  def create_notification_comment(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(item_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      item_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,16 @@ class User < ApplicationRecord
   def self.guest
     find_by(username: 'guest')
   end
+
+  def create_notification_follow(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification =current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
+    
 end
